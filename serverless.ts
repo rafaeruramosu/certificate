f06ee35c412034eb24a3e4e45e6d81a6a2f49430
Rafael <korewanihon@gmail.com>
2022-08-29 15:05:32 -0300

    feat: criando a function de gerar certificado

--- serverless.ts
+++ serverless.ts
@@ -3,7 +3,7 @@ import type { AWS } from '@serverless/typescript';
 const serverlessConfiguration: AWS = {
   service: 'certificate',
   frameworkVersion: '3',
-  plugins: ['serverless-esbuild', 'serverless-offline'],
+  plugins: ['serverless-esbuild', 'serverless-dynamodb-local', 'serverless-offline'],
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
@@ -17,13 +17,13 @@ const serverlessConfiguration: AWS = {
     },
   },
   functions: {
-    hello: {
-      handler: 'src/functions/hello.handler',
+    generateCertificate: {
+      handler: 'src/functions/generateCertificate.handler',
       events: [
         {
           http: {
-            path: 'hello',
-            method: 'GET',
+            path: 'generateCertificate',
+            method: 'post',
             cors: true
           }
         }
@@ -42,6 +42,14 @@ const serverlessConfiguration: AWS = {
       platform: 'node',
       concurrency: 10,
     },
+    dynamodb: {
+      stages: ['dev', 'local'],
+      start: {
+        port: 8000,
+        inMemory: true,
+        migrate: true
+      }
+    }
   },
   resources: {
     Resources: {
